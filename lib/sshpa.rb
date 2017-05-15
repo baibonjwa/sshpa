3d5e634b361e4a6252215bf1625fb37bf83527e8
happybai <happybyronbai@gmail.com>
2017-05-15 12:01:01 +0800

    v0.3.0

--- lib/sshpa.rb
+++ lib/sshpa.rb
@@ -1,29 +1,37 @@
-require "sshpa/version"
+require "./lib/sshpa/version.rb"
 require 'yaml'
 require 'optparse'
 
 module Sshpa
-  def self.hi *args
-    case args[0]
-    when '-v'
-      puts "Sshpa version is #{VERSION}"
-    when '--version'
-      puts "Sshpa version is #{VERSION}"
-    when 'init'
-      if File.exist?(ENV['HOME']+'/.sshpa.yml')
-        system 'echo ".sshpa.yml already exist."'
-      else
-        system 'echo ".sshpa.yml file has been created on $HOME."'
-        system "cp #{File.expand_path(File.dirname(__FILE__))}/sshpa/.sshpa.yml.example $HOME/.sshpa.yml"
+  def self.main
+    options = {}
+
+    opt_parser = OptionParser.new do |opt|
+      opt.banner = "Usage: sshpa COMMAND [OPTIONS]"
+      opt.separator  ""
+      opt.separator  "Commands"
+      opt.separator  "     add: add a server"
+      opt.separator  "     list: list servers"
+      opt.separator  "     remove: remove a server"
+      opt.separator  "     init: init sshpa config"
+      opt.separator  ""
+      opt.separator  "Options"
+
+      opt.on("-v","--version","") do |environment|
+        puts "v#{VERSION}"
+        exit
       end
-    when 'list'
-      if File.exist?(ENV['HOME']+'/.sshpa.yml')
-        file = open(ENV['HOME']+'/.sshpa.yml').read
-        puts file.to_yaml
-      else
-        puts '.sshpa.yml didn\'t exists.\n Please run \'sshpa init\' first.'
-      end
-    when 'add'
+
+      #opt.on("-f","--forever","runing on daemon mode?") do
+        #options[:daemon] = true
+      #end
+    end
+
+    opt_parser.parse!
+
+    case ARGV[0]
+    when "add"
+      puts "call start on options #{options.inspect}"
       if args.length != 5
         puts 'The number of argument is wrong.'
       else
@@ -41,28 +49,73 @@ module Sshpa
         File.write(ENV['HOME'] + '/.sshpa.yml', output)
         puts "The #{args[1]} has been added successfully"
       end
-    when 'remove'
+    when "list"
+      puts "call stop on options #{options.inspect}"
+      if File.exist?("#{ENV['HOME']}/.sshpa.yml")
+        file = open(ENV['HOME']+'/.sshpa.yml').read
+        puts file.to_yaml
+      else
+        puts '.sshpa.yml didn\'t exists.\n Please run \'sshpa init\' first.'
+      end
+    when "remove"
       file = open(ENV['HOME'] + '/.sshpa.yml').read
       config = YAML.load(file)
       puts config.delete(args[1]) { |el| "#{el} not found" }
       output = YAML.dump config
       File.write(ENV['HOME'] + '/.sshpa.yml', output)
       puts "The #{args[1]} has been removed successfully"
+    when "init"
+      if File.exist?(ENV['HOME']+'/.sshpa.yml')
+        system 'echo ".sshpa.yml already exist."'
+      else
+        system 'echo ".sshpa.yml file has been created on $HOME."'
+        system "cp #{File.expand_path(File.dirname(__FILE__))}/sshpa/.sshpa.yml.example $HOME/.sshpa.yml"
+      end
     else
-      begin
-        if File.exist?(ENV['HOME']+'/.sshpa.yml')
-          file = open(ENV['HOME']+'/.sshpa.yml').read
-          config = YAML.load(file)[args[0]]
-          command = "sshpass -e ssh -o StrictHostKeyChecking=no #{config['username']}\@#{config['host']}"
-          puts command
-          ENV['SSHPASS'] = config['password']
-          system command
-        else
-          puts '.sshpa.yml didn\'t exists.\n Please run \'sshpa init\' first.'
+      if (ARGV.length == 0)
+        puts opt_parser
+      else
+        begin
+          if File.exist?(ENV['HOME']+'/.sshpa.yml')
+            file = open(ENV['HOME']+'/.sshpa.yml').read
+            config = YAML.load(file)[ARGV[0]]
+            #config = YAML.load(file)
+            command = "sshpass -e ssh -o StrictHostKeyChecking=no #{config['username']}\@#{config['host']}"
+            puts command
+            ENV['SSHPASS'] = config['password']
+            system command
+          else
+            puts '.sshpa.yml didn\'t exists.\n Please run \'sshpa init\' first.'
+          end
+        rescue
+          puts 'Alias didn\'t exists or argument is not correct'
         end
-      rescue
-        puts 'Alias didn\'t exists or argument is not correct'
       end
     end
+
+    #case args[0]
+    #when 'remove'
+      #file = open(ENV['HOME'] + '/.sshpa.yml').read
+      #config = YAML.load(file)
+      #puts config.delete(args[1]) { |el| "#{el} not found" }
+      #output = YAML.dump config
+      #File.write(ENV['HOME'] + '/.sshpa.yml', output)
+      #puts "The #{args[1]} has been removed successfully"
+    #else
+      #begin
+        #if File.exist?(ENV['HOME']+'/.sshpa.yml')
+          #file = open(ENV['HOME']+'/.sshpa.yml').read
+          #config = YAML.load(file)[args[0]]
+          #command = "sshpass -e ssh -o StrictHostKeyChecking=no #{config['username']}\@#{config['host']}"
+          #puts command
+          #ENV['SSHPASS'] = config['password']
+          #system command
+        #else
+          #puts '.sshpa.yml didn\'t exists.\n Please run \'sshpa init\' first.'
+        #end
+      #rescue
+        #puts 'Alias didn\'t exists or argument is not correct'
+      #end
+    #end
   end
 end
--- sshpa.gemspec
+++ sshpa.gemspec
@@ -1,12 +1,11 @@
 # coding: utf-8
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'sshpa/version'
+require "./lib/sshpa/version.rb"
 
 Gem::Specification.new do |spec|
   spec.name          = "sshpa"
   spec.version       = Sshpa::VERSION
   spec.authors       = ["happybai"]
+  spec.date          = '2017-05-15'
   spec.email         = ["happybyronbai@gmail.com"]
 
   spec.summary       = %q{Server login management tool base on sshpass.}
@@ -14,19 +13,7 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/happybai/sshpa"
   spec.license       = "MIT"
 
-  # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
-  # delete this section to allow pushing this gem to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "https://rubygems.org"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
-  end
-
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.files         = ['lib/sshpa.rb', 'lib/sshpa/version.rb']
   spec.executables   = spec.files.grep(%r{^bin/*}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
-
-  spec.add_development_dependency "bundler", "~> 1.11"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
 end
